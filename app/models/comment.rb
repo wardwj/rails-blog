3acd2af9fef3d12b812f785b5c1d05119fc44294
LAPTOP-KOEF8UFO\willi <william.ward074@gmail.com>
2020-07-11 10:17:52 +0100

    Create comments for articles

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,3 @@
-class Article < ApplicationRecord
-  	has_many :comments
-  	validates :title, presence: true,
-                    length: { minimum: 5 }
+class Comment < ApplicationRecord
+    belongs_to :article
 end
